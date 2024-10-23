f370877dd9d38db967399ec3d26396e4b22cb948
chuang091 <thomas@ultralytics.com>
2024-10-24 02:29:32 +0800

    feat: map poc

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// 提供靜態文件
+app.use('/map', express.static(path.join(__dirname, 'map', 'build')));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
-})
+});
+
+// 新增 /map 路由
+app.get('/map', (req, res) => {
+  res.sendFile(path.join(__dirname, 'map', 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
-})
+});
---
+++ backend/map/src/Map.js
@@ -0,0 +1,90 @@
+import React, { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import L from 'leaflet';
+import * as turf from '@turf/turf';
+import 'leaflet/dist/leaflet.css';
+
+// HACK: 解決 React-Leaflet 在使用 MarkerClusterGroup 時的圖示問題
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+
+const Map = () => {
+  const InterpolatedLayer = () => {
+    const map = useMap();
+
+    useEffect(() => {
+      // 定義幾個示例點
+      const points = turf.featureCollection([
+        turf.point([121.5654, 25.0330], { value: 10 }),
+        turf.point([121.5680, 25.0350], { value: 20 }),
+        turf.point([121.5700, 25.0370], { value: 30 }),
+      ]);
+
+      // 使用 IDW (Inverse Distance Weighting) 進行空間內插法
+      const grid = turf.interpolate(points, 0.01, {
+        //gridType: 'points',
+        gridType: 'square',
+        property: 'value',
+        units: 'kilometers',
+      });
+      console.log(grid);
+
+      L.geoJson(grid, {
+        onEachFeature: function (feature, layer) {
+          if (feature.properties && feature.properties.value !== undefined) {
+            layer.bindPopup(feature.properties.value.toFixed(3).toString());
+          }
+        },
+        style: function (feature) {
+          return {
+            "color": getColor(feature.properties.value),
+            "opacity": 1,
+          }
+        }
+      }
+      ).addTo(map);
+      
+      //色階
+      function getColor(x) {
+        return x < 5 ? '#bd0026' :
+          x < 10 ? '#f03b20' :
+            x < 15 ? '#fd8d3c' :
+              x < 20 ? '#fecc5c' :
+                '#ffffb2';
+      };
+    }, [map]);
+
+    const iconUrl = 'assets/marker-icon.png';
+
+    return null;
+  };
+
+  return (
+    <MapContainer center={[25.0330, 121.5654]} zoom={13} style={{ height: '100vh', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <InterpolatedLayer />
+      {/* 添加示例點的標記 */}
+      <Marker position={[25.0330, 121.5654]}>
+        <Popup>點 1: 值 = 10</Popup>
+      </Marker>
+      <Marker position={[25.0350, 121.5680]}>
+        <Popup>點 2: 值 = 20</Popup>
+      </Marker>
+      <Marker position={[25.0370, 121.5700]}>
+        <Popup>點 3: 值 = 30</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
